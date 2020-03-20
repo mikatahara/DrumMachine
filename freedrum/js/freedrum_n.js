59626be7a9e6f68902f3131ab420b711d2a85409
masahiro kakishita <masahiro.kakishita@gmail.com>
2020-03-20 14:00:11 +0900

    freeDrum

--- freedrum/js/freedrum_n.js
+++ freedrum/js/freedrum_n.js
@@ -177,27 +177,27 @@ window.addEventListener('load', function (){
 					img_height/=2;
 				}
 
-				var xpos=(fdg1.cv.width-img_width*4-150)*0.25;
+				var xpos=6;;
 				var ypos=20;
 				mPosx[0]=xpos; mPosy[0]=ypos;
-				fdg1.fDrawImageW(mImg_pad[0],xpos,ypos,img_width,img_height); 
+				fdg1.fDrawImageWH(mImg_pad[0],xpos,ypos,img_width,img_height); 
 				xpos=xpos+img_width+50; mPosx[1]=xpos; mPosy[1]=ypos;
-				fdg1.fDrawImageW(mImg_pad[1],xpos,ypos,img_width,img_height);
+				fdg1.fDrawImageWH(mImg_pad[1],xpos,ypos,img_width,img_height);
 				xpos=xpos+img_width+50; mPosx[2]=xpos; mPosy[2]=ypos;
-				fdg1.fDrawImageW(mImg_pad[2],xpos,ypos,img_width,img_height);
+				fdg1.fDrawImageWH(mImg_pad[2],xpos,ypos,img_width,img_height);
 				xpos=xpos+img_width+50; mPosx[3]=xpos; mPosy[3]=ypos;
-				fdg1.fDrawImageW(mImg_pad[3],xpos,ypos,img_width,img_height);
+				fdg1.fDrawImageWH(mImg_pad[3],xpos,ypos,img_width,img_height);
 
-				xpos=(fdg1.cv.width-img_width*4-150)*0.75;
+				xpos=6;
 				ypos=ypos+img_height+10;
 				mPosx[4]=xpos; mPosy[4]=ypos;
-				fdg1.fDrawImageW(mImg_pad[4],xpos,ypos,img_width,img_height);
+				fdg1.fDrawImageWH(mImg_pad[4],xpos,ypos,img_width,img_height);
 				xpos=xpos+img_width+50; mPosx[5]=xpos; mPosy[5]=ypos;
- 				fdg1.fDrawImageW(mImg_pad[5],xpos,ypos,img_width,img_height);
+ 				fdg1.fDrawImageWH(mImg_pad[5],xpos,ypos,img_width,img_height);
 				xpos=xpos+img_width+50; mPosx[6]=xpos; mPosy[6]=ypos;
-				fdg1.fDrawImageW(mImg_pad[6],xpos,ypos,img_width,img_height);
+				fdg1.fDrawImageWH(mImg_pad[6],xpos,ypos,img_width,img_height);
 				xpos=xpos+img_width+50; mPosx[7]=xpos; mPosy[7]=ypos;
-				fdg1.fDrawImageW(mImg_pad[7],xpos,ypos,img_width,img_height);
+				fdg1.fDrawImageWH(mImg_pad[7],xpos,ypos,img_width,img_height);
 			}
 
 		}
